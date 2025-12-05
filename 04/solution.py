bd02e15ab6bf0cf2ff61e61e75b153ef3a72a495
Inias Peeters <inias.peeters@axxes.com>
2025-12-05 22:44:37 +0100

    Refactor ugly day 4 part 1

--- 04/solution.py
+++ 04/solution.py
@@ -3,46 +3,67 @@ from dataclasses import dataclass
 
 @dataclass
 class Floor:
-    rolls: list[list[bool]]
+    grid: list[list[bool]]
 
-    @staticmethod
-    def parse(lines: list[str]):
-        return Floor([[c == "@" for c in row.strip()] for row in lines])
+    def flat(self) -> list[bool]:
+        return [tile for row in self.grid for tile in row]
 
-    def count(self):
-        x_max = len(self.rolls)
-        y_max = len(self.rolls[0])
-        accessible_count = 0
-        accessible_map = [[False for _ in range(y_max)] for _ in range(x_max)]
-        for x in range(x_max):
-            for y in range(y_max):
-                val = self.rolls[x][y]
-                if not val:
-                    continue
-                surrounding_roll_count = 0
-                for x_offset in (-1, 0, 1):
-                    for y_offset in (-1, 0, 1):
-                        if x_offset == 0 and y_offset == 0:
-                            continue
-                        adj_x = x + x_offset
-                        adj_y = y + y_offset
-                        if adj_x < 0 or adj_y < 0 or adj_x >= x_max or adj_y >= y_max:
-                            continue
-                        if self.rolls[adj_x][adj_y]:
-                            surrounding_roll_count += 1
-                print(f"Surrounding roll count at ({x},{y}) is {surrounding_roll_count}")
-                if surrounding_roll_count < 4:
-                    accessible_count += 1
-                    accessible_map[x][y] = True
-                else:
-                    accessible_map[x][y] = False
-        for x in range(x_max):
-            for y in range(y_max):
-                print("x" if accessible_map[x][y] else ".", end="")
-            print()
-        return accessible_count
+    def count_true(self):
+        return sum(1 for t in self.flat() if t)
+
+    def at(self, x, y) -> bool:
+        return self.grid[x][y]
+
+    def x_max(self):
+        return len(self.grid)
+
+    def y_max(self):
+        return len(self.grid[0])
+
+    def neighbor_coords(self, x, y):
+        return [
+            (adj_x, adj_y)
+            for x_offset in (-1, 0, 1)
+            for y_offset in (-1, 0, 1)
+            if (
+                not (x_offset == 0 and y_offset == 0)
+                and (adj_x := x + x_offset) >= 0
+                and adj_x < self.x_max()
+                and (adj_y := y + y_offset) >= 0
+                and (adj_y < self.y_max())
+            )
+        ]
+
+    def all_coords(self):
+        return [(x, y) for x in range(self.x_max()) for y in range(self.y_max())]
+
+    def map(self, f):
+        return Floor(
+            [
+                [f(self.at(x, y), x, y) for y in range(self.y_max())]
+                for x in range(self.x_max())
+            ]
+        )
+
+    def neighbor_count(self, x, y):
+        return sum(1 for (nx, ny) in self.neighbor_coords(x, y) if self.at(nx, ny))
+
+    def __str__(self) -> str:
+        return "\n".join(
+            ["".join(["x" if t else "." for t in row]) for row in self.grid]
+        )
+
+
+def parse(lines: list[str]):
+    return Floor([[c == "@" for c in row.strip()] for row in lines])
 
 
 with open("04/input.txt", encoding="utf-8") as f:
-    floor = Floor.parse(f.readlines())
-    print(floor.count())
+    floor = parse(f.readlines())
+
+    # part 1
+    accessible_floor = floor.map(
+        lambda val, x, y: val and floor.neighbor_count(x, y) < 4
+    )
+    print(accessible_floor)
+    print(f"Part 1: {accessible_floor.count_true()}")
