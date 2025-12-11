6aa80cf8d57a11a1d6693fe73f244b6d1fe5a887
Inias Peeters <inias.peeters@external.engie.com>
2025-12-11 16:33:06 +0100

    Day 11 part 2

--- 11/solution.py
+++ 11/solution.py
@@ -9,20 +9,38 @@ def parse_val(line: str):
     return line.split(":")[1].strip().split(" ")
 
 
+def count_paths(connections: dict[str, list[str]], start: str, goal: str) -> int:
+    return do_count_paths(connections, start, goal, {})
+
+
+def do_count_paths(
+    connections: dict[str, list[str]],
+    start: str,
+    goal: str,
+    memo: dict[str, int],
+) -> int:
+    if start in memo:
+        return memo[start]
+    if start == goal:
+        return 1
+    neighbors = connections.get(start)
+    if not neighbors:
+        return 0
+    count = sum(do_count_paths(connections, n, goal, memo) for n in neighbors)
+    memo[start] = count
+    return count
+
+
 with open("11/input.txt") as f:
     lines = f.readlines()
     connections = {parse_key(l): parse_val(l) for l in lines}
-    count = 0
-    frontier = deque()
-    frontier.append("you")
-    while frontier:
-        current = frontier.popleft()
-        neighbors = connections.get(current)
-        if not neighbors:
-            continue
-        for n in neighbors:
-            frontier.append(n)
-            if n == "out":
-                count += 1
+    print(f"Part 1: {count_paths(connections, "you", "out")}")
 
-    print(count)
+    def cp(start, end):
+        return count_paths(connections, start, end)
+
+    part_2 = (
+        cp("svr", "fft") * cp("fft", "dac") * cp("dac", "out")
+        + cp( "svr", "dac") * cp("dac", "fft") * cp("fft", "out")
+    )
+    print(f"Part 2: {part_2}")
